efaa25986b713bfb45e22ce8ac2340c62cda2e92
Pedro Tocantis <pedro.tocantis@tandersolucoes.com>
2023-04-28 18:33:01 -0300

    readme commit

--- App.js
+++ App.js
@@ -17,12 +17,6 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingHorizontal: 20,
-    paddingVertical: 20,
-    marginTop: 20,
-  },
+
   
 });
